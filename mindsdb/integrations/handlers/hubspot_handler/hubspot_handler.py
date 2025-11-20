8214b2a539620d1c21c0e207a41ed9b0e2045fba
Gabriel Bressan <gabriel@talentify.io>
2025-11-20 15:42:01 -0400

    Add client_id and client_secret to refresh token request in HubSpot handler

--- mindsdb/integrations/handlers/hubspot_handler/hubspot_handler.py
+++ mindsdb/integrations/handlers/hubspot_handler/hubspot_handler.py
@@ -293,6 +293,8 @@ class HubspotHandler(APIHandler):
         data = {
             "grant_type": "refresh_token",
             "refresh_token": refresh_token,
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
         }
 
         try:
